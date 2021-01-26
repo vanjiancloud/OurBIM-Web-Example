f91407f2090b454daa23ad3d52fabdbfb5fe342d
woaidayouxi <8201612+woaidayouxi@user.noreply.gitee.com>
2021-01-26 17:29:33 +0800

    添加from表单

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,38 @@
 <template>
   <div>
-    <el-button type="warning">警告按钮</el-button>
-  <el-button type="danger" round>危险按钮</el-button>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="审批人">
+        <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-select v-model="formInline.region" placeholder="活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      formInline: {
+        user: '',
+        region: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
