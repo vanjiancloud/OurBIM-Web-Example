d0befcfc6d554298bfae53d8c81a49753cfe233c
woaidayouxi <8201612+woaidayouxi@user.noreply.gitee.com>
2021-01-27 15:43:52 +0800

    完成消息验证

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import router from './router'
 // 引入element
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 引入less
 import less from 'less'
+// 加载axios组件
+import axios from '@/utils/request.js'
 Vue.use(less)
 Vue.use(ElementUI)
+// 把axiox放到原型上
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
