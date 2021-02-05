3df31e259402625060f8116ccd0c68c73caf14ba
回头只需瞬间 <335212678@qq.com>
2021-02-05 11:53:41 +0800

    ..

--- src/views/preview/index.vue
+++ src/views/preview/index.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="box">
     <!-- 头部 -->
-    <my-header></my-header>
     <!-- 内容 -->
     <div class="content">
       <img src="./timg.jpg" alt="" class="img" />
     </div>
     <!-- 尾部 -->
-    <my-footer></my-footer>
   </div>
 </template>
 
 <script>
-import MyFooter from '../components/my-footer.vue'
-import myHeader from '../components/my-header.vue'
+
 export default {
-  components: { myHeader, MyFooter },
+
   data () {
     return {}
   },
