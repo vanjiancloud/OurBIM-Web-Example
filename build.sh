a355ea6df100a5088aad7f4f8806b5da13db6cfd
赵凯 <18730507026@163.com>
2021-02-20 16:53:49 +0800

    配置文件

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
-docker stop ourbim_web_system || true
 ###
  # @Author: zk
  # @Date: 2021-02-20 13:37:06
  # @LastEditors: zk
- # @LastEditTime: 2021-02-20 16:10:38
+ # @LastEditTime: 2021-02-20 16:53:40
  # @description: 
 ### 
 image_version=`date +%Y%m%d%H%M`;
@@ -12,10 +11,6 @@ echo $image_version;
 # git pull --rebase origin master;
 docker stop ourbim_web_system || true \;
 docker rm ourbim_web_system || true \;
-ls
-cd /mnt/web/docker/container/jenkins/jenkins_home/workspace/ourbim_web_system \;
 docker build  -t ourbim/system .;
 docker images;
-docker run -p 7012:80 -d --name ourbim_web_system -v /mnt/web/docker/container/jenkins/jenkins_home/workspace/ourbim_web_system/dist:/usr/share/nginx/html -v /mnt/web/docker/container/jenkins/jenkins_home/workspace/ourbim_web_system/nginx.conf:/etc/nginx/nginx.conf ourbim/system;
-# -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
-docker logs ourbim_web_system;
+docker run -p 7013:80 -d --name ourbim_web_system -v /mnt/web/docker/container/jenkins/jenkins_home/workspace/ourbim_web_system/dist:/usr/share/nginx/html -v /mnt/web/docker/container/jenkins/jenkins_home/workspace/ourbim_web_system/nginx.conf:/etc/nginx/nginx.conf ourbim/system;
