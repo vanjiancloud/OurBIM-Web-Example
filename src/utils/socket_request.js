df12d3f77d55b2c8605707eed5d51c32aa55f5ca
赵凯 <18730507026@163.com>
2021-03-10 17:30:15 +0800

    下拉选择

--- src/utils/socket_request.js
+++ src/utils/socket_request.js
@@ -2,10 +2,11 @@
  * @Author: zk
  * @Date: 2021-02-24 13:43:41
  * @LastEditors: zk
- * @LastEditTime: 2021-03-08 18:03:12
+ * @LastEditTime: 2021-03-10 16:04:22
  * @description: 
  */
-let WSPRO = "ws://1.180.74.210:11011/vjapi/";
+// let WSPRO = "ws://1.180.74.210:11011/vjapi/";
+let WSPRO = "ws://192.168.3.235:11011/vjapi/";
 export let BASE = {
     WSPRO
 }
