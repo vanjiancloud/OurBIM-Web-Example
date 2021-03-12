8d75f99a37793931187a338f119ece944ad0e900
回头只需瞬间 <335212678@qq.com>
2021-03-12 13:55:17 +0800

    修改

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,11 +8,8 @@
 // 把通用的一些东西放到里面以便在使用可以节约代码
 import axios from 'axios'
 import JSONBig from 'json-bigint'
-<<<<<<< HEAD
-=======
-// axios.defaults.baseURL = 'http://1.180.74.210:11011/vjapi'
->>>>>>> df12d3f77d55b2c8605707eed5d51c32aa55f5ca
-axios.defaults.baseURL = 'http://192.168.3.235:11011/vjapi'
+axios.defaults.baseURL = 'http://1.180.74.210:11011/vjapi'
+// axios.defaults.baseURL = 'http://192.168.3.151:11011/vjapi'
 axios.defaults.transformResponse = [function (data) {
   try {
     return JSONBig.parse(data)
