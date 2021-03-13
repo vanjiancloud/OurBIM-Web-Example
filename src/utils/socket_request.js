b5085dd0bdd6d58a3c79bd272a2ec0eeb93475ec
赵凯 <18730507026@163.com>
2021-03-13 09:58:15 +0800

    基地址切换

--- src/utils/socket_request.js
+++ src/utils/socket_request.js
@@ -2,11 +2,11 @@
  * @Author: zk
  * @Date: 2021-02-24 13:43:41
  * @LastEditors: zk
- * @LastEditTime: 2021-03-13 09:52:41
+ * @LastEditTime: 2021-03-13 09:56:07
  * @description: 
  */
-let WSPRO = "ws://1.180.74.210:11011/vjapi/";
-// let WSPRO = "ws://192.168.3.151:11011/vjapi/";
+// let WSPRO = "ws://1.180.74.210:11011/vjapi/";
+let WSPRO = "ws://192.168.3.235:11011/vjapi/";
 export let BASE = {
     WSPRO
 }
