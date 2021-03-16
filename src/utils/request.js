b5ddc0679b18f9cff6fa53c9c7a79c908bf9de88
赵凯 <18730507026@163.com>
2021-03-16 10:11:44 +0800

    基地址切换

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,14 +2,14 @@
  * @Author: zk
  * @Date: 2021-02-20 13:30:48
  * @LastEditors: zk
- * @LastEditTime: 2021-03-15 15:47:57
+ * @LastEditTime: 2021-03-16 10:00:44
  * @description: 
  */
 // 把通用的一些东西放到里面以便在使用可以节约代码
 import axios from 'axios'
 import JSONBig from 'json-bigint'
-axios.defaults.baseURL = 'http://1.180.74.210:11011/vjapi'
-// axios.defaults.baseURL = 'http://192.168.3.235:11011/vjapi'
+// axios.defaults.baseURL = 'http://1.180.74.210:11011/vjapi'
+axios.defaults.baseURL = 'http://192.168.3.151:11011/vjapi'
 axios.defaults.transformResponse = [function (data) {
   try {
     return JSONBig.parse(data)
--- src/utils/socket_request.js
+++ src/utils/socket_request.js
@@ -2,11 +2,11 @@
  * @Author: zk
  * @Date: 2021-02-24 13:43:41
  * @LastEditors: zk
- * @LastEditTime: 2021-03-15 17:51:35
+ * @LastEditTime: 2021-03-16 10:00:37
  * @description: 
  */
-let WSPRO = "ws://1.180.74.210:11011/vjapi/";
-// let WSPRO = "ws://192.168.3.151:11011/vjapi/";
+// let WSPRO = "ws://1.180.74.210:11011/vjapi/";
+let WSPRO = "ws://192.168.3.151:11011/vjapi/";
 export let BASE = {
     WSPRO
 }
