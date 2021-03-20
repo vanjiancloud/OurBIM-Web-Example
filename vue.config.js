2c819a501990cf2260a998a34fdfe9e3bc6f46a9
赵凯 <18730507026@163.com>
2021-03-20 11:38:26 +0800

    路径配置

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@
  * @Author: zk
  * @Date: 2021-03-20 11:07:12
  * @LastEditors: zk
- * @LastEditTime: 2021-03-20 11:28:06
+ * @LastEditTime: 2021-03-20 11:38:17
  * @description: 
  */
 module.exports = {
-    publicPath: "/usercenter/",
+    // publicPath: "/usercenter/",
     // 生产环境是否生成 sourceMap 文件
     productionSourceMap: false,
 }
