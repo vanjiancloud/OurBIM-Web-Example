f8bef029c503e47712a9440958de75468be63679
赵凯 <18730507026@163.com>
2021-03-20 11:55:28 +0800

    跳转页面

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@
  * @Author: zk
  * @Date: 2021-03-20 11:07:12
  * @LastEditors: zk
- * @LastEditTime: 2021-03-20 11:38:17
+ * @LastEditTime: 2021-03-20 11:54:36
  * @description: 
  */
 module.exports = {
-    // publicPath: "/usercenter/",
+    publicPath: "/usercenter/",
     // 生产环境是否生成 sourceMap 文件
     productionSourceMap: false,
 }
