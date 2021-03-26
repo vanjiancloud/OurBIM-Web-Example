6b40e983a456e759e17c34b8fae8ac9d414b154e
赵凯 <18730507026@163.com>
2021-03-26 17:59:59 +0800

    请求地址

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,15 +2,15 @@
  * @Author: zk
  * @Date: 2021-02-20 13:30:48
  * @LastEditors: zk
- * @LastEditTime: 2021-03-23 15:22:15
+ * @LastEditTime: 2021-03-26 17:59:39
  * @description:
  */
 // 把通用的一些东西放到里面以便重复使用
 import axios from 'axios'
 import JSONBig from 'json-bigint'
-// axios.defaults.baseURL = 'http://1.182.89.34:11011/vjapi'
+axios.defaults.baseURL = 'http://1.182.89.34:11011/vjapi'
 // axios.defaults.baseURL = 'http://192.168.3.235:11011/vjapi'
-axios.defaults.baseURL = 'http://192.168.3.151:11011/vjapi'
+// axios.defaults.baseURL = 'http://192.168.3.151:11011/vjapi'
 axios.defaults.transformResponse = [function (data) {
   try {
     return JSONBig.parse(data)
