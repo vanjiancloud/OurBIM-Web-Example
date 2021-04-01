ee4798e12e29828626fd765aa84ae1c02d7a35a3
赵凯 <18730507026@163.com>
2021-04-01 17:32:18 +0800

    默认值

--- src/components/web_client/view_cube.vue
+++ src/components/web_client/view_cube.vue
@@ -2,7 +2,7 @@
  * @Author: zk
  * @Date: 2021-03-08 09:27:06
  * @LastEditors: zk
- * @LastEditTime: 2021-04-01 17:18:25
+ * @LastEditTime: 2021-04-01 17:32:02
  * @description: 
 -->
 <template>
@@ -240,7 +240,7 @@ export default {
 }
 
 #box {
-  width: 35px;
+  width: 34px;
   height: 34px;
   position: relative;
   top: 0px;
