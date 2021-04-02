498d04311311054fe1745767d5e6ffa6c1e1f0ca
赵凯 <18730507026@163.com>
2021-04-02 11:54:01 +0800

    socket地址统一

--- src/utils/socket_request.js
+++ src/utils/socket_request.js
@@ -2,7 +2,7 @@
  * @Author: zk
  * @Date: 2021-02-24 13:43:41
  * @LastEditors: zk
- * @LastEditTime: 2021-04-02 10:53:15
+ * @LastEditTime: 2021-04-02 11:53:41
  * @description: 
  */
 // let WSPRO = "ws://1.182.89.34:11011/vjapi/";
