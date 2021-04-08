01b684d076aac4b71a945e571de93687b1125bbd
赵凯 <18730507026@163.com>
2021-04-08 16:08:42 +0800

    socket本地地址

--- src/utils/socket_request.js
+++ src/utils/socket_request.js
@@ -2,12 +2,12 @@
  * @Author: zk
  * @Date: 2021-02-24 13:43:41
  * @LastEditors: zk
- * @LastEditTime: 2021-04-07 10:13:34
+ * @LastEditTime: 2021-04-08 16:08:12
  * @description: 
  */
-// let WSPRO = "ws://1.182.89.34:11011/vjapi/";
+let WSPRO = "ws://1.182.89.34:11011/vjapi/";
 // let WSPRO = "ws://192.168.3.235:11011/vjapi/";
-let WSPRO = "ws://192.168.3.151:11011/vjapi/";
+// let WSPRO = "ws://192.168.3.151:11011/vjapi/";
 export let BASE = {
     WSPRO
 }
