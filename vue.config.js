92f268b12597685528b5ef32ccd8e6083aebcd6d
赵凯 <18730507026@163.com>
2021-04-08 10:29:10 +0800

    打包路径

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@
  * @Author: zk
  * @Date: 2021-03-20 11:07:12
  * @LastEditors: zk
- * @LastEditTime: 2021-03-20 12:31:49
+ * @LastEditTime: 2021-04-08 10:28:58
  * @description: 
  */
 module.exports = {
-    // publicPath: "/usercenter/",
+    publicPath: "/project_center/",
     // 生产环境是否生成 sourceMap 文件
     productionSourceMap: false,
 }
