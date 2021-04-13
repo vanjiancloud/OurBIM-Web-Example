55a67ba97783f3711c98209e0a274f704b28fb54
回头只需瞬间 <335212678@qq.com>
2021-04-13 09:57:37 +0800

    分享

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -242,25 +242,12 @@
               background-color="#fff"
             >
               <!-- 项目管理 -->
-              <el-submenu>
-                <template slot="title">
-                  <i class="el-icon-setting"></i>
-                  <span class="write">{{ $t('management') }}</span>
-                </template>
-                <!-- 我的项目 -->
-                <el-menu-item index="/manage">
-                  我的项目
-                </el-menu-item>
-                <!-- 模型集成 -->
-                <el-menu-item index="/code">
-                  模型集成
-                </el-menu-item>
-              </el-submenu>
+             
 
-              <!-- <el-menu-item index="/manage">
+              <el-menu-item index="/manage">
                 <i class="el-icon-setting"></i>
                 <span slot="title" class="write">{{ $t('management') }}</span>
-              </el-menu-item> -->
+              </el-menu-item>
               <!-- 创建项目 -->
               <el-menu-item index="/found">
                 <i class="el-icon-folder-add"></i>
