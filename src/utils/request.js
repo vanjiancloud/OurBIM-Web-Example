514a740b09c0a5969d9852455f309d24c3a5ef73
赵凯 <18730507026@163.com>
2021-04-13 10:04:56 +0800

    请求地址

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@
  * @Author: zk
  * @Date: 2021-02-20 13:30:48
  * @LastEditors: zk
- * @LastEditTime: 2021-04-12 10:07:56
+ * @LastEditTime: 2021-04-13 10:04:44
  * @description: 
  */
 import axios from 'axios'
