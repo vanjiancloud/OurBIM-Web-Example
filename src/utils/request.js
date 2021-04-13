4e309ed2ef549f1b03fced7e8f9a014891b95fb9
赵凯 <18730507026@163.com>
2021-04-13 10:35:58 +0800

    baseurl

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@
  * @Author: zk
  * @Date: 2021-02-20 13:30:48
  * @LastEditors: zk
- * @LastEditTime: 2021-04-08 16:07:56
+ * @LastEditTime: 2021-04-13 10:35:41
  * @description: 
  */
 
@@ -13,6 +13,7 @@ import { Getuserid } from '@/store/index.js'
 axios.defaults.baseURL = 'http://1.182.89.34:11011/vjapi'
 // axios.defaults.baseURL = 'http://192.168.3.235:11011/vjapi'
 // axios.defaults.baseURL = 'http://192.168.3.151:11011/vjapi'
+
 axios.defaults.transformResponse = [
   function (data) {
     try {
