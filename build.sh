4231a58cc59c041d3f5a36ae14c020b6bedb4b93
赵凯 <18730507026@163.com>
2021-06-05 18:06:28 +0800

    优化配置文件

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
  # @Author: zk
  # @Date: 2021-02-20 13:37:06
  # @LastEditors: zk
- # @LastEditTime: 2021-02-21 15:40:15
+ # @LastEditTime: 2021-06-05 18:06:17
  # @description: 
 image_version=`date +%Y%m%d%H%M`;
 echo $image_version;
 # cd vueServe
 # git pull --rebase origin master;
-docker stop ourbim_web_system || true \;
-docker rm ourbim_web_system || true \;
-docker build  -t ourbim/system .;
+docker stop beta_ourbim_web_system || true \;
+docker rm beta_ourbim_web_system || true \;
+docker build  -t ourbim/beta_system .;
 docker images;
-docker run -p 7012:80 -d --name ourbim_web_system -v /mnt/web/docker/container/jenkins/jenkins_home/workspace/ourbim-web-system/dist:/usr/share/nginx/html -v /mnt/web/docker/container/jenkins/jenkins_home/workspace/ourbim-web-system/nginx.conf:/etc/nginx/nginx.conf ourbim/system;
+docker run -p 7013:80 -d --name beta_ourbim_web_system -v /mnt/web/docker/container/jenkins/jenkins_home/workspace/test-ourbim-web-system/dist:/usr/share/nginx/html -v /mnt/web/docker/container/jenkins/jenkins_home/workspace/test-ourbim-web-system/nginx.conf:/etc/nginx/nginx.conf ourbim/beta_system;
