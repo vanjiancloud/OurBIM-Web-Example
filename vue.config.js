eeb35a208e0c2fe6e26f37d5532db9954911d80a
赵凯 <18730507026@163.com>
2021-09-10 11:32:30 +0800

    优化publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@
  * @Author: zk
  * @Date: 2021-03-20 11:07:12
  * @LastEditors: zk
- * @LastEditTime: 2021-09-02 16:01:02
+ * @LastEditTime: 2021-09-10 11:32:13
  * @description: 
  */
 module.exports = {
-    //  publicPath: "/project_center/",
+     publicPath: "/project_center/",
     // 生产环境是否生成 sourceMap 文件
     productionSourceMap: false,
 }
