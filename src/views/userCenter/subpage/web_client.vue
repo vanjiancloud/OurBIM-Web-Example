d2e7b1fc59bab56ea7f88f3ea828421437ef3e93
姚秋全 <1562590262@qq.com>
2023-03-10 14:21:25 +0800

    去除遮罩层

--- src/views/userCenter/subpage/web_client.vue
+++ src/views/userCenter/subpage/web_client.vue
@@ -17,7 +17,7 @@
       id="show-bim"
     ></iframe>
     <!-- 遮罩层 -->
-    <div
+    <!-- <div
       class="hidden-bim"
       :class="runTimeCode === 0 ? '' : 'phone-hidden-bim'"
       v-if="isFade"
@@ -56,7 +56,7 @@
           v-text="$t('webClient.loadBox.message[7]')"
         ></div>
       </div>
-    </div>
+    </div> -->
 
     <div class="systemDrawer">
       <el-drawer
